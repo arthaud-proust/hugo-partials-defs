a4ebc2e33f00efcb6b50b89c29701222837e47b8
arthaud-proust <arthaudp33@gmail.com>
2023-10-10 12:35:34 +0200

    fix: build dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
 import { partialsFoldersCommand } from './commands/partialsFolders';
 import { partialDefinitionProdiver } from './definitionProviders/partialDefinition';
-import { message } from './utils/message';
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.showInformationMessage(message('loaded'));
-
 	context.subscriptions.push(partialDefinitionProdiver);
 	context.subscriptions.push(partialsFoldersCommand);
 }
+
+export function deactivate() {}
